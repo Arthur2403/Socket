f0e90a54871b08e74c6d21306f090a4ecaaf0611
Arthur <arthurbej1@gmail.com>
2025-12-11 00:08:20 +0200

    Server1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Server;
+
+class Program
+{
+    static void Main()
+    {
+        TCPServer.StartServer();
+    }
+}
